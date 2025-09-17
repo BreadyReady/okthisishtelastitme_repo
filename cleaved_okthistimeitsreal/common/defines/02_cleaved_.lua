3a075c522777b65a4b53231a9036ed71bd2be5dd
BreadyReady <emarcus156@gmail.com>
2025-09-17 00:15:55 -0700

    Advisors, Companies, congress of mewni, and GFX

--- cleaved_okthistimeitsreal/common/defines/02_cleaved_.lua
+++ cleaved_okthistimeitsreal/common/defines/02_cleaved_.lua
@@ -1,4 +1,4 @@
-NDefines.NMilitary.BASE_FEMALE_DIVISIONAL_COMMANDER_CHANCE = 1
+NDefines.NMilitary.BASE_FEMALE_DIVISIONAL_COMMANDER_CHANCE = 0.5
 NDefines.NGame.START_DATE = "2018.1.1.12"
 NDefines.NGame.END_DATE = "2050.1.1.1"
 NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 1.517					-- No, we did not do the math for Cleaved. This is an estimate
